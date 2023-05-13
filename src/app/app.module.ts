9cd85257b8eb5bc7b14b959fcba838ccda4b74f0
nexoto-developer <62369981+Pana33@users.noreply.github.com>
2023-05-12 23:49:00 -0600

    Creacion de paginas
    Se crean las paginas que se tienen contempladas hasta el momento, todas cargadas en app-module, se procedara a ser cargadas por medio de lazy-loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
+import { RecordsComponent } from './pages/records/records.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainMenuComponent,
+    RecordsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
