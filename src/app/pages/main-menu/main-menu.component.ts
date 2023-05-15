7cbda0706e06e0ad3e6cd2f04e2e22feb0f4cbea
nexoto-developer <62369981+Pana33@users.noreply.github.com>
2023-05-14 22:15:35 -0600

    Revision de usuario en menu principal
    Se revisa el usuario en el menu principal, este es manejado con subscripciones para mantener su estado revisado en tiempo real

--- src/app/pages/main-menu/main-menu.component.ts
+++ src/app/pages/main-menu/main-menu.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { DocumentData } from 'firebase/firestore';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/authentication/auth.service';
+import { DatabaseService } from 'src/app/services/database/database.service';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent {
+export class MainMenuComponent implements OnInit, OnDestroy {
+
+  constructor(private auth: AuthService, private db: DatabaseService, private router: Router) { }
+
+  emailUser = ""
+  subEmailUser!: Subscription
+  dataUser!: DocumentData
+  subDataUser!: Subscription
+
+  ngOnInit(): void {
+    this.subEmailUser = this.auth.getEmailUser().subscribe(resEmailUser => {
+      if (resEmailUser != null && typeof (resEmailUser.email) == "string") {
+        this.emailUser = resEmailUser.email
+        this.subDataUser = this.db.getOneDocumentSubscribable("Users", this.emailUser).subscribe(resDataUser => {
+          this.dataUser = resDataUser
+          if (!this.dataUser["isActive"]) {
+            //El usuario no esta activo
+            this.logout()
+          }
+        })
+      } else {
+        //No encontramos su correo
+        this.logout()
+      }
+    })
+  }
+
+  logout() {
+    this.auth.logout()
+    this.router.navigate(['/login'])
+  }
+
+  ngOnDestroy(): void {
+    this.subEmailUser?.unsubscribe()
+    this.subDataUser?.unsubscribe()
+  }
 
 }
--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword, getAuth, signOut } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, user, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth:Auth) { }
+  constructor(private afAuth: Auth) { }
 
-  login(email:string,password:string){
-    return signInWithEmailAndPassword(this.afAuth,email,password)
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.afAuth, email, password)
   }
-    
+
+  getEmailUser() {
+    return user(this.afAuth)
+  }
+
+  logout() {
+    return signOut(this.afAuth)
+  }
+
 }
