303c32b6d28e8855c87c6bad43c25a5460f64430
nexoto-developer <62369981+Pana33@users.noreply.github.com>
2023-06-08 22:23:55 -0600

    Responsive de records
    Se hace el responsive de records agregando el boton de accion de la barra lateral y la configuracion del navbar

--- src/app/pages/records/module/records.module.ts
+++ src/app/pages/records/module/records.module.ts
@@ -5,6 +5,7 @@ import { RecordsRoutingModule } from './records-routing.module';
 import { RecordsComponent } from '../records.component';
 import { TableRecordsComponent } from '../components/table-records/table-records.component';
 import { ModelMapsComponent } from '../components/model-maps/model-maps.component';
+import { ButtonSideMenuModule } from 'src/app/shared/components/button-side-menu/button-side-menu.module';
 import { FormFilterModule } from 'src/app/shared/components/form-filter/form-filter.module';
 
 @NgModule({
@@ -17,6 +18,7 @@ import { FormFilterModule } from 'src/app/shared/components/form-filter/form-fil
     CommonModule,
     RecordsRoutingModule,
     FormFilterModule,
+    ButtonSideMenuModule,
   ]
 })
 export class RecordsModule { }
