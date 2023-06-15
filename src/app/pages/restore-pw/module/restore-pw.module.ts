92db81be8ff6a0e164b9d303330feaf0f8596777
nexoto-developer <62369981+Pana33@users.noreply.github.com>
2023-06-15 13:44:36 -0600

    Maquetado restore password
    Se crea la plantilla de html que se pretende utilizar para la pagina de cambio de contraseña, esta contiene 3 cards, una en caso de que el enlace sea valido, otra en caso de que ya se haya desechado y una mas que se mostrara en lo que se valida el enlace

--- src/app/pages/restore-pw/module/restore-pw.module.ts
+++ src/app/pages/restore-pw/module/restore-pw.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RestorePwRoutingModule } from './restore-pw-routing.module';
 import { RestorePwComponent } from '../restore-pw.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { RestorePwComponent } from '../restore-pw.component';
   ],
   imports: [
     CommonModule,
-    RestorePwRoutingModule
+    RestorePwRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class RestorePwModule { }
--- src/app/pages/restore-pw/restore-pw.component.ts
+++ src/app/pages/restore-pw/restore-pw.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-restore-pw',
   templateUrl: './restore-pw.component.html',
   styleUrls: ['./restore-pw.component.scss']
 })
-export class RestorePwComponent {
+export class RestorePwComponent implements OnInit{
 
+  constructor(private fb:FormBuilder){}
+
+  formRestorePw!:FormGroup
+  showSpinner:boolean = false
+  validLinkToRestorePw!:boolean
+
+  ngOnInit(): void {
+    this.formRestorePw = this.fb.group({
+      pass1:["",Validators.required],
+      pass2:["",Validators.required]
+    })
+    this.validLinkToRestorePw = true
+  }
+
+  changePw(){
+
+  }
+  
 }
